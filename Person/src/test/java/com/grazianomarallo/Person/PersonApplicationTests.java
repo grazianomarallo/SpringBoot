0996d8bdf7a2afa2350e1bd53293f7008ac8fc83
Graziano Marallo <grazianomarallo@Grazianos-MBP.home-life.hub>
2020-03-10 16:00:17 +0100

    Initiliase Person demo project for SpringBoot

---
+++ Person/src/test/java/com/grazianomarallo/Person/PersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grazianomarallo.Person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
